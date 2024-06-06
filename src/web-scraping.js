83ce874ef2380fffe6e313c46f71a42e2984e2ba
jesuswav <garciajesse2003@gmail.com>
2024-06-06 17:24:50 -0600

    get an object with the main information

--- src/web-scraping.js
+++ src/web-scraping.js
@@ -9,30 +9,74 @@ async function scrape() {
   // Inicializar el navegador Selenium (asegúrate de tener el WebDriver correspondiente)
   let driver = await new Builder().forBrowser('chrome').build()
 
-  let data = []
+  let post = {
+    description: '',
+    likes: '',
+    shared: '',
+    images: [],
+  }
+
+  const url1 = 'https://www.facebook.com/share/p/denWZ7obUeX4Kwzu/'
+  const url2 =
+    'https://www.facebook.com/linda.ruiz.54738/posts/pfbid02S3NGmbQn23WF4J6gaYq6sxREDnsiCjbkAWPaiuzw5kiyDcBXCQ9z88oN7XmtGvPl'
+  const url3 = 'https://www.facebook.com/share/p/E7ek6b1qzTR9racG/'
 
   try {
     // Abre una página web
-    await driver.get(
-      'https://www.facebook.com/UTCalvillo/posts/pfbid0uoBcN7NMvxqakZM8rg2RPwLJd6KgV323jrGkgQgdK8bkw71rxr9pwHFnUUo3TFzFl'
-    )
+    await driver.get(url1)
 
     await sleep(2000)
 
-    // Encuentra la imagen por su clase
-    let images = await driver.findElements(
-      By.css(
+    try {
+      const description = await getDescription(
+        driver,
+        'span.x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.xudqn12.x3x7a5m.x6prxxf.xvq8zen.xo1l8bm.xzsf02u.x1yc453h'
+      )
+      post.description = description
+    } catch (e) {
+      console.log(e)
+      console.log('Error al obtener la descripción')
+      post.description = 'Sin descripción'
+    }
+
+    try {
+      const images = await getImages(
+        driver,
         'img.xz74otr.x1ey2m1c.xds687c.x5yr21d.x10l6tqk.x17qophe.x13vifvy.xh8yej3'
       )
-    )
+      post.images = images
+    } catch (e) {
+      console.log(e)
+      console.log('Error al obtener las imagenes de la publicación')
+      post.images = []
+    }
 
-    for (let image of images) {
-      let url = await image.getAttribute('src')
-      data.push(url)
+    // Obtener los likes de la publicación
+    try {
+      // En caso de no tener ningún like el objeto que buscamos no existe
+      post.likes = await getLikes(
+        driver,
+        'span.xrbpyxo.x6ikm8r.x10wlt62.xlyipyv.x1exxlbk'
+      )
+    } catch {
+      console.log('Este post no ha sido compartido')
+      // como no existe le damos el valor de 0
+      post.likes = 0
+    }
+
+    // Saber cuantas veces se ha compartido esa publicación
+    try {
+      post.shared = await getShared(
+        driver,
+        'span.x193iq5w.xeuugli.x13faqbe.x1vvkbs.x1xmvt09.x1lliihq.x1s928wv.xhkezso.x1gmr53x.x1cpjm7i.x1fgarty.x1943h6x.xudqn12.x3x7a5m.x6prxxf.xvq8zen.xo1l8bm.xi81zsa'
+      )
+    } catch {
+      console.log('Este post no ha sido compartido')
+      post.shared = 0
     }
 
     // Imprime el valor de 'src'
-    console.log('URLs: ', data)
+    console.log('Post info:', post)
   } catch (error) {
     console.error('Error:', error)
   } finally {
@@ -42,3 +86,72 @@ async function scrape() {
 }
 
 scrape()
+
+// Obtener la descripción del posts
+const getDescription = async (driver, element) => {
+  // Elemento que contiene la descripción
+  let description = await driver.findElement(By.css(element)).getText()
+
+  const textDescription = description.replace(/\n/g, ' ')
+
+  return textDescription
+}
+
+// Función para saber cuantos likes tiene una publicación
+const getLikes = async (driver, element) => {
+  // Obtener los likes de la publicación
+  let likes = await driver.findElement(By.css(element)).getText()
+
+  // Comprobamos si la publicación tiene más de mil likes
+  const thousands = likes.slice(-3)
+
+  // Obtenemos solo el número de likes, evitanto la nomenclatura
+  let likesNumber = parseInt(likes.match(/\d+/g).join(''))
+
+  // En caso de tener > 1000 likes multiplicamos por mil, el número que nos da la regular expression
+  if (thousands === 'mil') {
+    likesNumber = likesNumber * 1000
+    console.log('Holaaaaaa')
+  }
+
+  return likesNumber
+}
+
+// Función para obtener las veces que ha sido compartida una publicación
+const getShared = async (driver, element) => {
+  let sharedElements = await driver.findElements(By.css(element))
+
+  const shared = await sharedElements[2].getText()
+
+  const sharedNumber = shared.match(/\d+/g)
+
+  let finalShared = parseInt(sharedNumber.join(''))
+
+  return finalShared
+}
+
+// Función para obtener las imagenes de una publicación
+const getImages = async (driver, element) => {
+  // Obtener las imagenes que contenga la publicación
+  let images = []
+  let imagesFromWeb
+
+  // Encuentra la imagen por su clase
+  imagesFromWeb = await driver.findElements(By.css(element))
+
+  // En caso de ser una publicación con una sola foto
+  if (imagesFromWeb.length === 0) {
+    imagesFromWeb = await driver.findElements(
+      By.css(
+        'img.x1ey2m1c.xds687c.x5yr21d.x10l6tqk.x17qophe.x13vifvy.xh8yej3.xl1xv1r'
+      )
+    )
+  }
+
+  for (let image of imagesFromWeb) {
+    let url = await image.getAttribute('src')
+    images.push(url)
+  }
+
+  return images
+}
